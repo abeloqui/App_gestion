7fd4c8e521e826ffd113e00499272e65fb9b0e01
B.André <71408583+abeloqui@users.noreply.github.com>
2026-04-07 21:45:21 -0300

    Update app_gestion.py

--- app_gestion.py
+++ app_gestion.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from database import init_db
 
+from database import init_db
+init_db()
 st.set_page_config(page_title="Sistema Pro", layout="wide")
 
 if "logged_in" not in st.session_state:
